8c04f7b02934554aa58df6f4db0a57744f206770
duzhengwei <duzhengwei@3ncto.com>
2017-03-16 12:22:21 +0800

    dddd

---
+++ Front/upload/upload.php
@@ -0,0 +1,40 @@
+<?php
+
+class Uplod{
+    protected $size;
+    protected $type;
+    protected $allow_type;
+
+    public function setSize($size)
+    {
+        $this->size=$size;
+    }
+
+    public function checkSize($config)
+    {
+        if ($this->size>$config['size']) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+if ( !empty( $_FILES ) ) {
+
+    $tempPath = $_FILES[ 'file' ][ 'tmp_name' ];
+    $uploadPath = './uploads' . DIRECTORY_SEPARATOR . $_FILES[ 'file' ][ 'name' ];
+
+    move_uploaded_file( $tempPath, $uploadPath );
+    $answer = [
+        'fielePath' => $uploadPath,
+        'msg' => 'ok'
+    ];
+    $json = json_encode($answer);
+    echo $json;
+
+} else {
+    echo 'No files';
+}
+
+?>
